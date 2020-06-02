8fa001551a8dcdcfa4eca7e5535bef3dc7737f50
pBarsky <patrykbaranski12@gmail.com>
2020-06-02 15:52:22 +0200

    MVC template added

---
+++ PizzeriaTNAI.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PizzeriaTNAI.UI.Startup))]
+namespace PizzeriaTNAI.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

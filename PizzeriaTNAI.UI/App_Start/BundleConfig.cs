efbd29fd926b82d4633d70449ce7bf432f1891f6
pBarsky <patrykbaranski12@gmail.com>
2020-06-22 15:41:02 +0200

    OrderDetails update

--- PizzeriaTNAI.UI/App_Start/BundleConfig.cs
+++ PizzeriaTNAI.UI/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace PizzeriaTNAI.UI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style.css"));
         }
     }
 }
